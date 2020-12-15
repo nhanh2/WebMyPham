9bc56a87f18d8b9cb9840011a7bcc2e4f578e8d4
nhanh2 <misa.nhanh2@gmail.com>
2020-12-15 23:54:46 +0700

    Hoàn thành

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    var cart_count = 0 ;
+  console.log("ready!");
+  
+
+  $(".card").click(function () {
+    window.location.href = "productdetail.html";
+  });
+
+  $("#shopping-card").click(function () {
+    window.location.href = ' ShoppingCardDetail.html';
+  });
+
+
+});
